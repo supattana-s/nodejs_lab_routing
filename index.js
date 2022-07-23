216722d1e582e1997c87b7fcf3ad39303a655951
supattana-s <supattana.sue@gmail.com>
2022-07-23 10:22:46 +0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('test for push after created 2 upstream')
+const express = require('express');
+const { get } = require('http');
+
+const app = express();
+
+
+app.get('/todos', (req, res) => {
+    res.json({ "message": `${req.method} todos` })
+});
+
+app.post('/todos', (req, res) => {
+    res.json({ "message": `${req.method} todos` })
+});
+
+app.put('/todos', (req, res) => {
+    res.json({ "message": `${req.method} todos` })
+});
+
+app.patch('/todos', (req, res) => {
+    res.json({ "message": `${req.method} todos` })
+});
+
+app.delete('/todos', (req, res) => {
+    res.json({ "message": `${req.method} todos` })
+});
+
+
+
+app.listen(8000, () => console.log("Server run at port: 8000"));
