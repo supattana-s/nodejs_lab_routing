b9d6e1fecace661b9675c995d3102c08385b9c13
supattana-s <supattana.sue@gmail.com>
2022-07-26 11:42:58 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('test for push after created 2 upstream')
+const express = require('express')
+
+const app = express()
+
+app.get('/redirect', (req, res) => {
+    res.redirect('http://google.com')
+})
+
+
+app.listen(8000, () => console.log("Server is running on port: 8000"))
