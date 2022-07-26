b1618cf819ebd3de44823847ba3dbfb2dfdddd60
supattana-s <supattana.sue@gmail.com>
2022-07-26 14:07:44 +0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('test for push after created 2 upstream')
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>Welcome to My website</h1>')
+})
+
+app.get('/home', (req, res) => {
+    res.status(200).send('<h1>This is home page</h1>')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
+
+app.listen(8000, () => console.log('Server running on port: 8000'))
