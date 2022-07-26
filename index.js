6410c3fd68245c67fbeb04523519dd06e63ae82f
supattana-s <supattana.sue@gmail.com>
2022-07-26 14:22:49 +0700

    Done

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const fs = require('fs/promises')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('<h1>Welcome to My website</h1>')
+app.get('/', async (req, res) => {
+    const data = await fs.readFile('./page/index.html', 'utf-8')
+    res.status(200).send(data)
 })
 
-app.get('/home', (req, res) => {
-    res.status(200).send('<h1>This is home page</h1>')
+app.get('/home', async (req, res) => {
+    const data = await fs.readFile('./page/home.html', 'utf-8')
+    res.status(200).send(data)
 })
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+app.get('*', async (req, res) => {
+    const data = await fs.readFile('./page/notFound.html', 'utf-8')
+    res.status(404).send(data)
 })
 
 app.listen(8000, () => console.log('Server running on port: 8000'))
