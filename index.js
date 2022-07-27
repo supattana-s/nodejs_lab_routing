67b3585bfee2eb4cad170b71e87cd3f75cc86215
supattana-s <supattana.sue@gmail.com>
2022-07-27 13:38:22 +0700

    Done with YT, 03052022 PEarth

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('test for push after created 2 upstream')
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/page/index.html'));
+    console.log(__dirname)
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/page/logIn.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/page/register.html'));
+});
+
+app.post('/submit-login', (req, res) => {
+    res.redirect('/')
+});
+
+app.post('/submit-register', (req, res) => {
+    res.redirect('/')
+});
+
+
+app.listen(8000, () => console.log('Server running on port: 8000'));
